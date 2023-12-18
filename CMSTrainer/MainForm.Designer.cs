39b523a370b761997318e8a7effba8b0d8aea74f
DiyRex <dsdissanayaka2002@gmail.com>
2023-12-18 20:45:52 +0530

    Add project files.

---
+++ CMSTrainer/MainForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CMSTrainer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtcr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtcurrent = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblstatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(134, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add CR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtcr
+            // 
+            this.txtcr.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtcr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcr.Location = new System.Drawing.Point(144, 124);
+            this.txtcr.Name = "txtcr";
+            this.txtcr.Size = new System.Drawing.Size(124, 26);
+            this.txtcr.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(94, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CR :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(318, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Car Mechanic Simulator 2021";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Available CR :";
+            // 
+            // txtcurrent
+            // 
+            this.txtcurrent.AutoSize = true;
+            this.txtcurrent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcurrent.Location = new System.Drawing.Point(162, 89);
+            this.txtcurrent.Name = "txtcurrent";
+            this.txtcurrent.Size = new System.Drawing.Size(44, 20);
+            this.txtcurrent.TabIndex = 5;
+            this.txtcurrent.Text = "CR :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Game Status :";
+            // 
+            // lblstatus
+            // 
+            this.lblstatus.AutoSize = true;
+            this.lblstatus.Location = new System.Drawing.Point(90, 244);
+            this.lblstatus.Name = "lblstatus";
+            this.lblstatus.Size = new System.Drawing.Size(0, 13);
+            this.lblstatus.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 269);
+            this.Controls.Add(this.lblstatus);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtcurrent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtcr);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtcr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txtcurrent;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblstatus;
+    }
+}
+
---
+++ CMSTrainer/MainForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+
+namespace CMSTrainer
+{
+    public partial class MainForm : Form
+    {
+        Mem m = new Mem();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string money = txtcr.Text;
+            m.WriteMemory("UnityPlayer.dll+0x019F1170,C60,40,B8,90,38,2D8,D60", "int", money);
+            txtcurrent.Text = money;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int PID = m.GetProcIdFromName("Car Mechanic Simulator 2021");
+            if (PID > 0)
+            {
+                m.OpenProcess(PID);
+                lblstatus.Text = "Running !";
+                lblstatus.ForeColor = Color.Green;
+                float memo = m.ReadInt("UnityPlayer.dll+0x019F1170,C60,40,B8,90,38,2D8,D60");
+                txtcurrent.Text = memo.ToString();
+            }
+            else
+            {
+                lblstatus.Text = "Not Running !";
+                lblstatus.ForeColor = Color.Red;
+            }
+        }
+    }
+}
